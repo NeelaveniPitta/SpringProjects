e4f4a5ad4f52ef4872a470721e1d7b8003644c11
NEELAVENI <NEELAVENI@DESKTOP-5E78GQT>
2025-06-06 10:59:48 +0530

    init commit JDBC Connection

---
+++ SpringBoot_JDBCApp_1/src/main/java/com/ashi/SpringBootJdbcApp1Application.java
@@ -0,0 +1,13 @@
+package com.ashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcApp1Application.class, args);
+	}
+
+}
